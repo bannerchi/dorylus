ffcc9ed65700069c83644364660f5e49787002f7
ronchi <ron.chi@vidaxl.com>
2016-05-04 11:46:43 +0800

    some change for job bugs

--- jobs/job.go
+++ jobs/job.go
@@ -6,9 +6,8 @@ import (
 	"github.com/bannerchi/dorylus/models"
 	"os/exec"
 	"runtime/debug"
-	//"strings"
+//"strings"
 	"log"
-	"sync"
 	"time"
 )
 
@@ -18,8 +17,7 @@ type Job struct {
 	name       string
 	task       *models.Task
 	runFunc    func(time.Duration) (string, string, error, bool)
-	running    sync.Mutex
-	status     int
+	status     int // >0 is running
 	Concurrent bool
 	pid        int
 }
@@ -77,18 +75,19 @@ func (j *Job) GetPid() int {
 }
 
 func (j *Job) Run() {
+
+	if !j.Concurrent && j.status > 0 {
+		log.Printf("任务[%d]上一次执行尚未结束，本次被忽略。\n", j.id)
+		return
+	}
+
 	defer func() {
 		if err := recover(); err != nil {
 			log.Println(err, "\n", string(debug.Stack()))
 		}
 	}()
 
-	t := time.Now()
 
-	if j.Concurrent {
-		j.running.Lock()
-		defer j.running.Unlock()
-	}
 
 	if workPool != nil {
 		workPool <- true
@@ -97,11 +96,13 @@ func (j *Job) Run() {
 		}()
 	}
 
-	j.status = 1
+	log.Printf("开始执行任务: %d\n", j.id)
+	j.status++
 	defer func() {
-		j.status = 0
+		j.status--
 	}()
 
+	t := time.Now()
 	timeout := time.Duration(time.Hour * 24)
 	if j.task.Timeout > 0 {
 		timeout = time.Second * time.Duration(j.task.Timeout)
@@ -123,7 +124,7 @@ func (j *Job) Run() {
 
 	if isTimeout {
 		tasklogs.Status = models.TASK_TIMEOUT
-		tasklogs.Error = fmt.Sprintf("Task run over %d sec\n----------------------\n%s\n", int(timeout/time.Second), cmdErr)
+		tasklogs.Error = fmt.Sprintf("Task run over %d sec\n----------------------\n%s\n", int(timeout / time.Second), cmdErr)
 	} else if err != nil {
 		tasklogs.Status = models.TASK_ERROR
 		tasklogs.Error = err.Error() + ":" + cmdErr
@@ -135,43 +136,4 @@ func (j *Job) Run() {
 	j.task.ExecuteTimes++
 	j.task.RunServer = GetLocalIp()
 	models.UpdateTask(j.task.Id, j.task)
-
-	// 发送邮件通知
-	//	if (j.task.Notify == 1 && err != nil) || j.task.Notify == 2 {
-	//		user, uerr := models.UserGetById(j.task.UserId)
-	//		if uerr != nil {
-	//			return
-	//		}
-	//
-	//		var title string
-	//
-	//		data := make(map[string]interface{})
-	//		data["task_id"] = j.task.Id
-	//		data["username"] = user.UserName
-	//		data["task_name"] = j.task.TaskName
-	//		data["start_time"] = beego.Date(t, "Y-m-d H:i:s")
-	//		data["process_time"] = float64(ut) / 1000
-	//		data["output"] = cmdOut
-	//
-	//		if isTimeout {
-	//			title = fmt.Sprintf("任务执行结果通知 #%d: %s", j.task.Id, "超时")
-	//			data["status"] = fmt.Sprintf("超时（%d秒）", int(timeout/time.Second))
-	//		} else if err != nil {
-	//			title = fmt.Sprintf("任务执行结果通知 #%d: %s", j.task.Id, "失败")
-	//			data["status"] = "失败（" + err.Error() + "）"
-	//		} else {
-	//			title = fmt.Sprintf("任务执行结果通知 #%d: %s", j.task.Id, "成功")
-	//			data["status"] = "成功"
-	//		}
-	//
-	//		content := new(bytes.Buffer)
-	//		mailTpl.Execute(content, data)
-	//		ccList := make([]string, 0)
-	//		if j.task.NotifyEmail != "" {
-	//			ccList = strings.Split(j.task.NotifyEmail, "\n")
-	//		}
-	//		if !mail.SendMail(user.Email, user.UserName, title, content.String(), ccList) {
-	//			beego.Error("发送邮件超时：", user.Email)
-	//		}
-	//	}
 }
